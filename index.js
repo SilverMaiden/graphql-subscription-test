3122fd72466f634efd8e51ce66e56cb210c0be28
Jeffrey Burt <hi@jeffreyburt.com>
2020-10-23 19:52:34 -0400

    update for traces

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const { resolvers, typeDefs } = require('./schema');
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  // cors: false,
+  // introspection: false,
+  subscriptions: {
+    onConnect: (connectionParams) => {
+      console.log('hello', connectionParams)
+    }
+  },
   plugins: [ApolloServerPluginInlineTrace()],
 });
 
 server.listen().then(({ url, subscriptionsUrl }) => {
   console.log(`🚀 Server ready at ${url}`);
   console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
+  console.log('🚀 Explore your graph at https://studio.apollographql.com/dev')
 })
